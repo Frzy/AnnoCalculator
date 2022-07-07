b24d7eeba51981e6e41679f2a6f12bac7ce36e0c
Matthew Fry <Frzy84@gmail.com>
2022-07-07 14:48:08 -0700

    set up for github pages serving static files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
+const debug = process.env.NODE_ENV !== "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+  //assetPrefix: '',
+  assetPrefix: !debug ? "/AnnoCalculator/" : "",
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    // Important: return the modified config
+    return config;
+  } /*,
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // console.log('webpackDevMiddleware');
+    // console.log(config);
+    // Important: return the modified config
+    return config
+  }, */,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
