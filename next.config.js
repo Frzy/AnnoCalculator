f130e7cdb8a37dda61c2c51c492e9e243cfdcce6
Matthew Fry <Frzy84@gmail.com>
2022-07-07 18:18:13 -0700

    update deployment strategy

--- next.config.js
+++ next.config.js
@@ -1,40 +1,10 @@
-const debug = process.env.NODE_ENV !== 'production'
+module.exports = function () {
+  const isProd = process.env.NODE_ENV === 'production'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-    }
-  },
-  //assetPrefix: '',
-  assetPrefix: !debug ? '/AnnoCalculator/' : '',
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map((rule) => {
-      if (rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  } /*,
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */,
+  console.log({ isProd })
+
+  return {
+    basePath: isProd ? '/AnnoCalculator' : undefined,
+    assetPrefix: isProd ? '/AnnoCalculator/' : undefined,
+  }
 }
-
-module.exports = nextConfig
