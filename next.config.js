f474a39101f690e99ddf0a54743c2d3b8a9c60d4
Matthew Fry <Frzy84@gmail.com>
2022-07-07 18:23:47 -0700

    add output folder

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-  const isProd = process.env.NODE_ENV === 'production'
-
-  return {
-    basePath: isProd ? '/AnnoCalculator' : undefined,
-    assetPrefix: isProd ? '/AnnoCalculator/' : undefined,
-  }
-}
