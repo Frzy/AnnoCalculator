0334adc85fa73eaa238e84dc8300405e3a15e214
Matthew Fry <Frzy84@gmail.com>
2022-07-07 18:22:54 -0700

    remove out folder

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = function () {
   const isProd = process.env.NODE_ENV === 'production'
 
-  console.log({ isProd })
-
   return {
     basePath: isProd ? '/AnnoCalculator' : undefined,
     assetPrefix: isProd ? '/AnnoCalculator/' : undefined,
